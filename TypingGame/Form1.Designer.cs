fcfd56b2f1c01f1d0523e1387e60e1b38bb91445
john80513 <john80513@hotmail.com>
2019-01-20 18:48:08 +0800

    git init

---
+++ TypingGame/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace TypingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.CorrctLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.MissedLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.TotalLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.AccuracyLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("新細明體", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 96;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.MultiColumn = true;
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(856, 157);
+            this.listBox1.TabIndex = 0;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 800;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.CorrctLabel,
+            this.MissedLabel,
+            this.TotalLabel,
+            this.AccuracyLabel,
+            this.toolStripStatusLabel5,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 135);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(856, 22);
+            this.statusStrip1.SizingGrip = false;
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // CorrctLabel
+            // 
+            this.CorrctLabel.Name = "CorrctLabel";
+            this.CorrctLabel.Size = new System.Drawing.Size(58, 17);
+            this.CorrctLabel.Text = "Correct:0";
+            // 
+            // MissedLabel
+            // 
+            this.MissedLabel.Name = "MissedLabel";
+            this.MissedLabel.Size = new System.Drawing.Size(57, 17);
+            this.MissedLabel.Text = "Missed:0";
+            // 
+            // TotalLabel
+            // 
+            this.TotalLabel.Name = "TotalLabel";
+            this.TotalLabel.Size = new System.Drawing.Size(46, 17);
+            this.TotalLabel.Text = "Total:0";
+            // 
+            // AccuracyLabel
+            // 
+            this.AccuracyLabel.Name = "AccuracyLabel";
+            this.AccuracyLabel.Size = new System.Drawing.Size(78, 17);
+            this.AccuracyLabel.Text = "Accuracy:0%";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(469, 17);
+            this.toolStripStatusLabel5.Spring = true;
+            this.toolStripStatusLabel5.Text = "Difficulty";
+            this.toolStripStatusLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Maximum = 701;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(769, 109);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(856, 157);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel CorrctLabel;
+        private System.Windows.Forms.ToolStripStatusLabel MissedLabel;
+        private System.Windows.Forms.ToolStripStatusLabel TotalLabel;
+        private System.Windows.Forms.ToolStripStatusLabel AccuracyLabel;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ TypingGame/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TypingGame
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Stats stats = new Stats();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listBox1.Items.Add((Keys)random.Next(65, 90));//65~90對應鍵盤上A~Z的英文字母
+            if (listBox1.Items.Count > 7)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.Add("Game Over!");
+                timer1.Stop();
+                button1.Visible = true;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Contains(e.KeyCode))//驗證是否有鍵盤輸入得值
+            {
+                listBox1.Items.Remove(e.KeyCode);//移除玩家輸入正確的值
+                //listBox1.Refresh();
+
+                //增加字母出現速度
+                if (timer1.Interval > 400)
+                    timer1.Interval -= 10;
+                else if (timer1.Interval > 250)
+                    timer1.Interval -= 7;
+                else if (timer1.Interval > 100)
+                {
+                    timer1.Interval -= 2;
+                }
+                toolStripProgressBar1.Value = 800 - timer1.Interval;
+                stats.Update(true);
+            }
+            else {
+                stats.Update(false);
+            }
+
+            CorrctLabel.Text = "Correct:" + stats.Correct;
+            MissedLabel.Text = "Missed:" + stats.Missed;
+            TotalLabel.Text = "Total:" + stats.Total;
+            AccuracyLabel.Text = "Accuracy:" + stats.Accuracy+"%";
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            timer1.Interval = 800;
+            toolStripProgressBar1.Value = 800- timer1.Interval;
+            timer1.Start();
+        }
+    }
+}
